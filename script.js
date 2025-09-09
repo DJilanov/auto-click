706aef2a47138f9b130b015722ac8b7f81e7be14
Dimitar Jilanov <djilanov@gmail.com>
2025-09-09 10:57:50 +0300

    Add randomness

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 import { mouse, Button } from "@nut-tree-fork/nut-js";
 
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const randomBetween = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
 async function autoClick() {
   while (true) {
     await mouse.click(Button.LEFT);
     console.log("Clicked at", new Date().toISOString());
-    await new Promise((resolve) => setTimeout(resolve, 3000)); // 3 seconds
+
+    const delay = randomBetween(3000, 5000); // 3–5 sec
+    console.log(`Waiting ${delay / 1000} seconds...`);
+    await sleep(delay);
   }
 }
 
