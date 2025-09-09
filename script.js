a0ce0354b16d63b15f6c18a1b9535f2a82f5637d
Dimitar Jilanov <djilanov@gmail.com>
2025-09-09 10:56:59 +0300

    Fixes

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-import { mouse, Button } from "@nut-tree/nut-js";
+import { mouse, Button } from "@nut-tree-fork/nut-js";
 
 async function autoClick() {
   while (true) {
     await mouse.click(Button.LEFT);
-    console.log("Clicked!");
-    await new Promise(resolve => setTimeout(resolve, 3000)); // wait 3 sec
+    console.log("Clicked at", new Date().toISOString());
+    await new Promise((resolve) => setTimeout(resolve, 3000)); // 3 seconds
   }
 }
 
-autoClick();
+autoClick().catch((err) => {
+  console.error("Error in autoClick:", err);
+});
